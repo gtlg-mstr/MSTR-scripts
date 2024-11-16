7f2c12345d8345edbfbacf60551144a68dca7cc0
Gareth <46423422+gtlg-mstr@users.noreply.github.com>
2024-11-15 20:31:07 -0500

    Create AuditUsers1.py

---
+++ Archive/AuditUsers1.py
@@ -0,0 +1,87 @@
+"""
+Audit current license usage
+---------------------------
+
+
+"""
+#Import required modules
+from typing import List
+from mstrio.connection import Connection, get_connection
+from mstrio.users_and_groups import list_users
+import pandas as pd
+
+# Define your MicroStrategy server connection details when connecting outside of Workstation
+# This requires a project_id (newer versions can work off the name)
+base_url = "https://env-XXXX.customer.cloud.microstrategy.com/MicroStrategyLibrary/"
+username = "mstr"
+password = ""
+project_id = "0DDDDEC8C94B320B4E93498C1EE98D18"
+
+# Connect to the MicroStrategy server - Not workstation
+conn = Connection(base_url, username, password, project_id=project_id)
+
+def list_active_user_privileges(connection: "Connection") -> List[dict]:
+    """List user privileges for all active users.
+
+    Args:
+        connection: MicroStrategy connection object returned by
+            `connection.Connection()`
+
+    Returns:
+        list of dicts where each of them is in given form:
+        {
+            'id' - id of user
+            'name' - name of user
+            'username' - username of user
+            'enabled' - if the user is active or not
+            'privileges' - list of privileges of user
+        }
+    """
+    group_value=''
+    security_role=''
+    project=''
+    all_users = list_users(connection=connection)
+    active_users = [u for u in all_users if u.enabled]
+    privileges_list = []
+    for usr in all_users:
+        p = {
+            'id': usr.id,
+            'name': usr.name,
+            'username': usr.username,
+            'enabled': usr.enabled,
+            'privileges': usr.privileges,
+        }
+        for prvlg in p['privileges']:
+            for source in prvlg['sources']:
+                if 'group' in source:
+                    group_value = source['group']['name']
+                if 'securityRole' in source:
+                    security_role = source['securityRole']['name']
+                if 'project' in source:
+                    project = source['project']['name'] 
+                p = [usr.id,usr.name,usr.username,usr.enabled,prvlg['privilege']['name'],group_value,security_role,project]            
+                privileges_list.append(p)
+    return privileges_list
+
+
+# list of privileges for all users
+print('Retrieving list of privileges for all users ')
+list1=list_active_user_privileges(conn)
+#print(list1)
+
+
+# Define column names for Dataframe
+columns = ['userid', 'userfullname','username','enabled','prvlg','groupname','secrole','project']
+
+# Create a DataFrame based on list and column names
+df = pd.DataFrame(list1, columns=columns)
+
+# Display the DataFrame (optional)
+#print(df)
+
+#Close connection to MicroStrategy (Important or it will keep additional sessions open)
+conn.close()
+
+#Export to Excel (can be changed to CSV)
+print('Exporting to excel - ** file will dump to current working directory with name: licenses.xlsx **')
+df.to_excel('licenses.xlsx', sheet_name='Sheet1')
